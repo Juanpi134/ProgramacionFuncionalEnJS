bd52c4e094f806dc28550d02caf54187c417f959
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-05 11:46:47 -0300

    se agrega ejercicio con map

--- app.js
+++ app.js
@@ -2,9 +2,39 @@
 //en donde debería recorrer y almacenarlo en un nuevo array
 const numeros = [1,2,3];
 
-const duplicados = [];
-
+//const duplicados = [];
+/*
 for (let i = 0; i < numeros.length; i++) {
-    numeros[i] * 2;
+    numeros[i] = numeros[i] * 2
     duplicados.push(numeros[i]);
 }
+*/
+//de esta forma es como uno tiene que hacer otro paso 
+
+//console.log(duplicados)
+
+const ul = document.querySelector("ul");
+
+//utilizando map se puede hacer de otra forma
+//no necesite de utilizar más pasos para mostrarlo
+/*
+const duplicados2 = numeros.map((numero) => {
+    return numero * 2
+})
+*/
+
+//console.log(duplicados2)
+
+const elementos = numeros.map((elemento) => {
+    const li = document.createElement("li");
+
+    console.log(elemento); //1,2,3
+
+    li.textContent = elemento;
+
+    ul.appendChild(li);
+
+    document.body.appendChild(ul);
+})
+
+
