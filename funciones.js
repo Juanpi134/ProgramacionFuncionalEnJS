2ab8640e9e83d859d8346f755a8ed3e0f2b979fa
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-05 11:49:13 -0300

    se añade archivo funciones.js

---
+++ funciones.js
@@ -0,0 +1,36 @@
+// en javascript una funcion es un valor
+/*  
+
+const saludar = function(){
+    console.log("Hola");
+}
+
+o esto
+
+
+const saludar = () => {
+    console.log("Hola");
+}
+
+y luego se la llama
+
+saludar();
+
+se puedne guardar variables en funciones
+const sumar = (a,b) => a+b;
+
+se pueden poner funciones dentro de un array
+
+const operaciones = [
+    (a,b)=>a+b,
+    (a,b)=>a-b
+];
+
+o dentro de un objeto
+
+const calculadora = {
+    sumar:(a,b)=>a+b,
+    restar:(a,b)=>a-b
+}
+
+*/
