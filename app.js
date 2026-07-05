b189340d1e2b83260aacc44c2ae57fc93fe960e3
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-05 11:36:09 -0300

    se añade el archivo index.html

---
+++ app.js
@@ -0,0 +1,10 @@
+// Forma imperativa de un arreglo
+//en donde debería recorrer y almacenarlo en un nuevo array
+const numeros = [1,2,3];
+
+const duplicados = [];
+
+for (let i = 0; i < numeros.length; i++) {
+    numeros[i] * 2;
+    duplicados.push(numeros[i]);
+}
